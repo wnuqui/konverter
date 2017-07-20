40826170f18b1733cd68f77af96261504b0c17e2
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-07-20 19:04:41 +0800

    Fix failing controller specs that checks creation of Conversion record

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,5 +1,18 @@
 class ConversionsController < ApplicationController
   def create
-    render json: {}
+    now = Time.now
+    conversion = Conversion.where({ created_at: (now - 1.minute)..now }).where(post_params.slice(:base, :target)).first
+
+    if conversion.nil?
+      Conversion.create(post_params.slice(:base, :target))
+    end
+
+    render json: { conversion: '1 US dollar = 0.8927 euros' }
+  end
+
+  private
+
+  def post_params
+    params.permit!
   end
 end
