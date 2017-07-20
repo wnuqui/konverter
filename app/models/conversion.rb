6584cabb6b38aabef26a695b89c98c44a9171fab
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-07-20 19:13:59 +0800

    Reusing snippet from other source: https://github.com/wnuqui/cm

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,2 +1,23 @@
 class Conversion < ApplicationRecord
+  URL = 'https://www.google.com/search?q=BASE+AMOUNT+to+TARGET'.freeze
+
+  class << self
+    def convert_currency_url(options)
+      url = URL
+            .gsub(/BASE/, options[:base])
+            .gsub(/TARGET/, options[:target])
+
+      options[:base_amount] ? url.gsub(/AMOUNT/, options[:base_amount]) : url.gsub(/AMOUNT\+/, '')
+    end
+
+    def convert_via_google(options)
+      agent = Mechanize.new
+      page  = agent.get(convert_currency_url(options))
+      doc   = page.parser
+
+      result = doc.xpath('//*[@id="ires"]/ol/table').children.children[2].children.children[0].children.text
+
+      result
+    end
+  end
 end
