8510297d2bb5d45fabac62e59abee95e976aaa6e
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-07-30 07:55:22 +0800

    Fix test descriptions

--- spec/routing/conversion_spec.rb
+++ spec/routing/conversion_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'routes', :type => :routing do
-  describe '#foo' do
-    it 'bar' do
+RSpec.describe 'routes', type: :routing do
+  describe 'GET /conversions?base=USD&target=EURO' do
+    it 'routes' do
       expect(post: '/conversions?base=USD&target=EURO').to route_to(
         controller: 'conversions',
         action: 'create',
