fe6065daf0c0aba08024ee28e5f375d9e4cf8a0f
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-07-30 09:30:17 +0800

    Record conversion call

--- spec/controllers/conversions_controller_spec.rb
+++ spec/controllers/conversions_controller_spec.rb
@@ -3,19 +3,25 @@ require 'rails_helper'
 RSpec.describe ConversionsController, type: :controller do
   include ActiveSupport::Testing::TimeHelpers
 
+  around(type: :http) do |example|
+    VCR.use_cassette('conversion') do
+      example.run
+    end
+  end
+
   let(:params) do
     { base: 'USD', target: 'EURO', format: :json  }
   end
 
   describe 'POST create' do
-    it 'has a 200 status code' do
+    it 'has a 200 status code', type: :http do
       post :create, params: params
       expect(response).to be_http_200
     end
 
     context 'JSON response' do
       context 'html accept type' do
-        it 'responds JSON content' do
+        it 'responds JSON content', type: :http do
           post :create, params: { base: 'USD', target: 'EURO', format: :html  }
           expect(response.content_type).to_not eq('text/html')
           expect(response.content_type).to eq('application/json')
@@ -23,14 +29,14 @@ RSpec.describe ConversionsController, type: :controller do
       end
 
       context 'json accept type' do
-        it 'responds JSON content' do
+        it 'responds JSON content', type: :http do
           post :create, params: params
           expect(response.content_type).to eq('application/json')
         end
       end
 
       context 'default accept type' do
-        it 'responds JSON content' do
+        it 'responds JSON content', type: :http do
           post :create, params: { base: 'USD', target: 'EURO'  }
           expect(response.content_type).to eq('application/json')
         end
